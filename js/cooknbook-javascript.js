93308868cd6f0f73d9d23e1c782acc0d57e03bf1
SabineWanka <sabinewanka@yahoo.de>
2019-10-29 21:32:59 +0100

    add API

--- js/cooknbook-javascript.js
+++ js/cooknbook-javascript.js
@@ -1,1 +1,12 @@
-console.log("hello")
+
+var cusineType = $("#cusine-input").val() || "asian";
+var queryURL = "https://api.edamam.com/search?q=" + cusineType + "&app_id=$ae6bf7b0&app_key=$f2c047350509b09ed7dc98cd15dd86d1—"
+    ;
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    $("#recipe-view").text(JSON.stringify(response));
+    console.log(response);
+
+});
