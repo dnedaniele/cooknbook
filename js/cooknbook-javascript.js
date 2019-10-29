491e00937134c733089179e60f682e517686be83
Pablo Fernandez <pfdzm@users.noreply.github.com>
2019-10-29 20:13:26 +0100

    add map and display a marker with a label

--- js/cooknbook-javascript.js
+++ js/cooknbook-javascript.js
@@ -1,5 +1,19 @@
-console.log("test conflict")
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+// create a Google Map on the page and display markers on it
+
+// const GOOGLE_MAPS_JS_API = "AIzaSyDNkDcVt_FLVyQCfckYAKP2YaGrkaFdmKc";
+
+var map;
+
+var berlin = { lat: 52.52, lng: 13.405 };
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: berlin,
+    zoom: 13
+  });
+  var marker = new google.maps.Marker({
+    position: { lat: 52.515884, lng: 13.401693 },
+    map: map,
+    label: "ESMT Coding Bootcamp"
+  });
 }
