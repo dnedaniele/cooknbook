b080297732b15bc2603ca52e4d7c4043eaf37b9b
Pablo Fernandez <pfdzm@users.noreply.github.com>
2019-10-30 16:08:26 +0100

    Remove Google Maps in favor of MapBox and Leaflet maps, which do not place restrictions on usage

--- js/cooknbook-javascript.js
+++ js/cooknbook-javascript.js
@@ -1,21 +1,22 @@
 var cusineType = $("#cusine-input").val() || "asian";
-var queryURL = "https://api.edamam.com/search?q=" + cusineType + "&app_id=$ae6bf7b0&app_key=$f2c047350509b09ed7dc98cd15dd86d1—"
-    ;
+var queryURL =
+  "https://api.edamam.com/search?q=" +
+  cusineType +
+  "&app_id=$ae6bf7b0&app_key=$f2c047350509b09ed7dc98cd15dd86d1—";
 $.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    $("#recipe-view").text(JSON.stringify(response));
-    console.log(response);
-
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  $("#recipe-view").text(JSON.stringify(response));
+  console.log(response);
 });
 
 // create a Google Map on the page and display markers on it
 // const GOOGLE_MAPS_JS_API = "AIzaSyDNkDcVt_FLVyQCfckYAKP2YaGrkaFdmKc";
 
-var map;
-
 const BERLIN_COORD = { lat: 52.52, lng: 13.405 };
+
+var user = {};
 
 // this function returns the longitude and latitude of the user's location
 // we should consider triggering this only when the user clicks on the 'search'
@@ -24,7 +25,10 @@ function getUserLocation() {
   if ("geolocation" in navigator) {
     /* geolocation is available */
     navigator.geolocation.getCurrentPosition(function(position) {
-      console.log(position.coords.latitude, position.coords.longitude);
+      user.lat = position.coords.latitude;
+      user.lng = position.coords.longitude;
+      renderMapbox();
+      renderMapLeaflet();
     });
   } else {
     /* geolocation IS NOT available */
@@ -32,15 +36,35 @@ function getUserLocation() {
   }
 }
 
+getUserLocation();
+
 // this function paints a google map on the page, with a labeled marker
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: BERLIN_COORD,
-    zoom: 13
-  });
-  var marker = new google.maps.Marker({
-    position: { lat: 52.515884, lng: 13.401693 },
-    map: map,
-    label: "ESMT Coding Bootcamp"
+// trying Leaflet JS for maps
+
+function renderMapLeaflet() {
+  var mymap = L.map("mapid").setView([user.lat, user.lng], 13);
+
+  L.tileLayer(
+    `https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}`,
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      id: "mapbox.streets",
+      accessToken:
+        "pk.eyJ1IjoicGZkem0iLCJhIjoiY2syZDZyaXZ6MHQxbjNqcDR4Nm5jMDlkaiJ9.NfZcJYx2UKi8cUdQalfkyg"
+    }
+  ).addTo(mymap);
+}
+
+// Also trying Mapbox GL JS
+
+function renderMapbox() {
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoicGZkem0iLCJhIjoiY2syZDZyaXZ6MHQxbjNqcDR4Nm5jMDlkaiJ9.NfZcJYx2UKi8cUdQalfkyg";
+  var map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/dark-v9?optimize=true",
+    center: [user.lng, user.lat],
+    zoom: 12
   });
 }
